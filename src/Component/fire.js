fce208a5e69313e644527d9f97860dd04f49ec59
LunchBox <Vickel1993@Hotmail.com>
2019-02-11 20:43:51 -0500

    hide api key

--- src/Component/fire.js
+++ src/Component/fire.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyADlfl3q261VQcdKK-BqDl7HkrLNEfjE38",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "password-generator-db07b.firebaseapp.com",
     databaseURL: "https://password-generator-db07b.firebaseio.com",
     projectId: "password-generator-db07b",
@@ -11,16 +11,4 @@ const config = {
 const fire = firebase.initializeApp(config);
 export default fire;
 
-// import firebase from 'firebase';
-
-// const config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
-//   apiKey: 'AIzaSyD_PxFjzcPKX50pq3ITq6Q69fuHvCowMtE',
-//   authDomain: 'visual-studio-sync.firebaseapp.com',
-//   databaseURL: 'https://visual-studio-sync.firebaseio.com',
-//   projectId: 'visual-studio-sync',
-//   storageBucket: 'visual-studio-sync.appspot.com',
-//   messagingSenderId: '691628632398'
-// };
-// const fire = firebase.initializeApp(config);
-// export default fire;
 
