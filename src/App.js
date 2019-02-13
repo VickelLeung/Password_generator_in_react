53c15a5409ed240aba67cf21708694e1b94b43a1
LunchBox <Vickel1993@Hotmail.com>
2019-02-13 02:36:11 -0500

    added display info

--- src/App.js
+++ src/App.js
@@ -15,15 +15,13 @@ class App extends Component {
     user: null
   }
 
-  // authListener = this.authListener.bind(this);
-
   componentDidMount() {
     this.authListener();
   }
 
   authListener = () => {
     fire.auth().onAuthStateChanged((user) => {
-      console.log(user);
+      // console.log(user);
       if (user) {
         this.setState({ user });
         localStorage.setItem('user', user.uid);
